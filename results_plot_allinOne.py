47974d58667622786f3dfa5af311e6650b63b735
jeffensen <36547049+jeffensen@users.noreply.github.com>
2022-09-22 11:53:58 +0200

    Minor adjustments to format of datafile and some aesthetical refinements

--- results_plot_allinOne.py
+++ results_plot_allinOne.py
@@ -6,53 +6,46 @@ Created on Sat Sep 26 17:30:41 2020
 """
 
 import matplotlib.pyplot as plt
-from os import name, getcwd
+from os import getcwd, path
 import pandas as pd
 import seaborn as sns
 
-# CONSTANTS
-currentdir = getcwd()
-if name == 'nt':
-    #windows paths
-    datadir = currentdir + '\\data\\'
-if name == 'posix':
-    #linux paths
-    datadir = currentdir + '/data/'
+
+# set data directory as relative path from this files directory
+reppath = getcwd()
+datadir = path.join(reppath,"data")
     
-datafile = datadir + "SAT_singleMiniblocks.xlsx"
+datafile = path.join(datadir, "SAT_singleMiniblocks.csv")
 
 grouplabels = ["young", "old"]
 noiselabels = ["$\it{low~noise}$","$\it{high~noise}$"]
 stepslabels = ["$\it{2~steps}$","$\it{3~steps}$"]
 tasklabels  = ["$\it{SAT}$","$\it{SAW}$","$\it{SWM}$","$\it{IDP}$"]
 paramlabels = ["$\it{alpha}$","$\it{beta}$","$\it{theta}$"]
-groupcolors = ["dodgerblue","r"]
+groupcolors = ["dodgerblue","red"]
 subjectplotcolor = "grey"
 
 # help function to set outer lines of violinplot
 def patch_violinplot(axis):
      from matplotlib.collections import PolyCollection
      ax = axis
+     isya = True
      for art in ax.get_children():
           if isinstance(art, PolyCollection):
-              art.set_edgecolor("white")
+              if isya:
+                  art.set_edgecolor(groupcolors[0])
+                  isya = False
+              else:
+                  art.set_edgecolor(groupcolors[1])
+                  isya = True
 
 
 #%% import data
-df = pd.read_excel(datafile)
+df = pd.read_csv(datafile, index_col=0, dtype={'ID':object})
 
 # scale conditions as binary indicators
 df["noise"]=df["noise"] - df["noise"].min()
 df["steps"]=df["steps"] - df["steps"].min()
-
-# # convert Covariate RTs from ms to seconds
-# df['SAW_RT']=df['SAW_RT'] / 1000
-# df['SWM_RT']=df['SWM_RT'] / 1000
-# df['IDP_RT']=df['IDP_RT'] / 1000
-
-# add colum for SAT performance (% of maximum possible points)
-maxPoints = 2009   # maximum possible points gain from 10.000 depth3-agent simulations
-df['SAT_PER'] = df['Total_points'] / maxPoints * 100
 
 
 #%% plot main plot (MeanPD, Performance, RTs, Model Params)
@@ -88,42 +81,43 @@ for a in fig.axes:
     labelsize=labelsize)   
 
 # calc subject-wise means 
-dfsubs = df.groupby('Subject_ID').mean().reset_index()
-dfsubsNoise = df.groupby(['Subject_ID','noise']).mean().reset_index()
-dfsubsSteps = df.groupby(['Subject_ID','steps']).mean().reset_index()
+dfsubs = df.groupby('ID').mean().reset_index()
+dfsubsNoise = df.groupby(['ID','noise']).mean().reset_index()
+dfsubsSteps = df.groupby(['ID','steps']).mean().reset_index()
 subsCount = len(dfsubs)
+subsIDs = pd.unique(dfsubs.ID)
 
 # plot row1
 #------------------------
 
 ycol = 'MeanPD'
 ylabel = 'Mean Planning Depth'
-ylims = [0.8,3]
+ylims = [0.8,2.99]
 xlabels = ['','',''] #['Age Group', 'Noise Level', 'Number of Steps']
 
-sns.barplot(  ax=ax00, x="Group", y=ycol, data=dfsubs, palette=groupcolors, ci=95, n_boot=10000, capsize=.2)
-sns.stripplot(ax=ax00, x="Group", y=ycol, data=dfsubs, color='black', jitter=0.1, alpha=0.3)
+sns.barplot(  ax=ax00, x="group", y=ycol, data=dfsubs, palette=groupcolors, ci=95, n_boot=10000, capsize=.2)
+sns.stripplot(ax=ax00, x="group", y=ycol, data=dfsubs, color='black', jitter=0.1, alpha=0.3)
 
 
 for t in range(subsCount):
-    c = groupcolors[int(dfsubsNoise[dfsubsNoise.Subject_Nr==t+1].Group.min())]
-    sns.lineplot(ax=ax01, x="noise", y=ycol, data=dfsubsNoise[dfsubsNoise.Subject_Nr==t+1], color=c, linewidth=0.6, alpha=0.4)
-    sns.stripplot(ax=ax01, x="noise",y=ycol, data=dfsubsNoise[dfsubsNoise.Subject_Nr==t+1], color=c, alpha=0.4, jitter=0, size=3)
+    c = groupcolors[int(dfsubsNoise[dfsubsNoise.ID==subsIDs[t]].group.min())]
+    sns.lineplot(ax=ax01, x="noise", y=ycol, data=dfsubsNoise[dfsubsNoise.ID==subsIDs[t]], color=c, linewidth=0.6, alpha=0.4)
+    sns.stripplot(ax=ax01, x="noise",y=ycol, data=dfsubsNoise[dfsubsNoise.ID==subsIDs[t]], color=c, alpha=0.4, jitter=0, size=3)
     plt.setp(ax01.lines, zorder=1)
     plt.setp(ax01.collections, zorder=1)
-sns.pointplot(ax=ax01, x="noise", y=ycol, data=dfsubsNoise, palette=groupcolors, hue="Group", ci=95, n_boot=10000, capsize=.2, scale=1.5)
-ax01.get_legend().remove()
+sns.pointplot(ax=ax01, x="noise", y=ycol, data=dfsubsNoise, palette=groupcolors, hue="group", ci=95, n_boot=10000, capsize=.2, scale=1.5)
+current_handles, current_labels = ax01.get_legend_handles_labels()
+ax01.legend(current_handles, grouplabels, fontsize=legendsize, title_fontsize=legendsize, loc='upper left', title="Age Group")
 
   
 for t in range(subsCount): 
-    c = groupcolors[int(dfsubsSteps[dfsubsSteps.Subject_Nr==t+1].Group.min())]
-    sns.lineplot(ax=ax02, x="steps", y=ycol, data=dfsubsSteps[dfsubsSteps.Subject_Nr==t+1], color=c, linewidth=0.6, alpha=0.4)
-    sns.stripplot(ax=ax02, x="steps",y=ycol, data=dfsubsSteps[dfsubsSteps.Subject_Nr==t+1], color=c, alpha=0.4, jitter=0, size=3)
+    c = groupcolors[int(dfsubsSteps[dfsubsSteps.ID==subsIDs[t]].group.min())]
+    sns.lineplot(ax=ax02, x="steps", y=ycol, data=dfsubsSteps[dfsubsSteps.ID==subsIDs[t]], color=c, linewidth=0.6, alpha=0.4)
+    sns.stripplot(ax=ax02, x="steps",y=ycol, data=dfsubsSteps[dfsubsSteps.ID==subsIDs[t]], color=c, alpha=0.4, jitter=0, size=3)
     plt.setp(ax02.lines, zorder=1)
     plt.setp(ax02.collections, zorder=1)
-sns.pointplot(ax=ax02, x="steps", y=ycol, data=dfsubsSteps, palette=groupcolors, hue="Group", ci=95, n_boot=10000, capsize=.2, scale=1.5) 
-current_handles, current_labels = ax02.get_legend_handles_labels()
-ax02.legend(current_handles, grouplabels, fontsize=legendsize, title_fontsize=legendsize, loc='upper left', title="Age Group")
+sns.pointplot(ax=ax02, x="steps", y=ycol, data=dfsubsSteps, palette=groupcolors, hue="group", ci=95, n_boot=10000, capsize=.2, scale=1.5) 
+ax02.get_legend().remove()
 
 
 # adjust plots
@@ -134,7 +128,6 @@ for t in range(3):
     axs_top[t].set_xlabel(xlabels[t], fontsize=labelsize)
     axs_top[t].set_ylim(ylims[0],ylims[1])
 ax00.set_xticks([],[])
-#ax00.set_xticklabels(["",""])
 ax01.set_xticklabels(noiselabels)
 ax01.tick_params(axis="x",direction="in",pad=-28)
 ax02.set_xticklabels(stepslabels)
@@ -152,13 +145,13 @@ covLegend = []
 ylabel = 'Performance (%)'
 datacols = ['SAT_PER','SAW_PER','SWM_PER','IDP_PER']
 cCount = len(datacols)
-ylims = [-25,125]
+ylims = [-25,135]
 
 # convert from wide to long format with Performance
-dfsubsL = pd.melt(dfsubs, id_vars=['Subject_ID','Group'], value_vars=datacols,
+dfsubsL = pd.melt(dfsubs, id_vars=['ID','group'], value_vars=datacols,
              var_name='Task', value_name='Performance')
 
-sns.violinplot(ax=axbig1, x="Task", y="Performance", hue="Group", split=True, data=dfsubsL, palette=groupcolors)
+sns.violinplot(ax=axbig1, x="Task", y="Performance", hue="group", split=True, data=dfsubsL, palette=groupcolors)
 patch_violinplot(axbig1)
 axbig1.get_legend().remove()
 
@@ -168,39 +161,35 @@ axbig1.tick_params(axis="x",direction="inout")
 axbig1.set_xticklabels(["","","",""])
 axbig1.set_ylim(ylims[0],ylims[1])
 
-#current_handles, current_labels = axbig1.get_legend_handles_labels()
-#axbig1.legend(current_handles, grouplabels, fontsize=legendsize, title_fontsize=legendsize, title="Age Group")
-
 
 # plot row3: plot for RT and Covariate RTs
 #---------------------------------------------
 xlabel = ""
 covLegend = []
 ylabel = 'RT (s)'
-datacols0 = ['RT_1st','SAW_RT']
+datacols0 = ['SAT_RT','SAW_RT']
 datacols1 = ['SWM_RT','IDP_RT']
-ylims0 = [-2,27]
-ylims1 = [-0.5,6.5]
+ylims0 = [-5,29]
+ylims1 = [-0.5,7.]
 
 # convert from wide to long format with Performance
-dfsubsL = pd.melt(dfsubs, id_vars=['Subject_ID','Group'], value_vars=datacols0,
+dfsubsL = pd.melt(dfsubs, id_vars=['ID','group'], value_vars=datacols0,
              var_name='Task', value_name='RT')
 # plot
-sns.violinplot(ax=axmed20, x="Task", y="RT", hue="Group", split=True, data=dfsubsL, palette=groupcolors)
+sns.violinplot(ax=axmed20, x="Task", y="RT", hue="group", split=True, data=dfsubsL, palette=groupcolors)
 patch_violinplot(axmed20)
 axmed20.get_legend().remove()
 
 # convert from wide to long format with Performance
-dfsubsL = pd.melt(dfsubs, id_vars=['Subject_ID','Group'], value_vars=datacols1,
+dfsubsL = pd.melt(dfsubs, id_vars=['ID','group'], value_vars=datacols1,
              var_name='Task', value_name='RT')
 # plot
-sns.violinplot(ax=axmed21, x="Task", y="RT", hue="Group", split=True, data=dfsubsL, palette=groupcolors)
+sns.violinplot(ax=axmed21, x="Task", y="RT", hue="group", split=True, data=dfsubsL, palette=groupcolors)
 patch_violinplot(axmed21)
 axmed21.get_legend().remove()
 
 axmed20.set_ylabel(ylabel, fontsize=labelsize) 
 axmed21.set_ylabel("") 
-#axmed21.tick_params(axis="y",direction="in", pad = -18)
 axmed20.set_xlabel(xlabel, fontsize=labelsize)
 axmed21.set_xlabel("")
 axmed20.tick_params(axis="x",direction="inout", bottom=False, labelbottom=False, top=True, labeltop=True, pad = -22) 
@@ -210,25 +199,18 @@ axmed21.set_xticklabels(tasklabels[2:])
 
 axmed20.set_ylim(ylims0[0],ylims0[1])
 axmed21.set_ylim(ylims1[0],ylims1[1])
-#current_handles, current_labels = axmed20.get_legend_handles_labels()
-#axmed20.legend(current_handles, grouplabels, fontsize=legendsize, title_fontsize=legendsize, title="Age Group")
 
 
 # plot row4: model params (alpha, beta, theta)
 #----------------------------------------------------------
 ylabel = 'Value'
-datacols = ['Model_alpha','Model_beta','Model_theta']
-ylims = [[ -.07, .23],
-         [-0.5 ,4.7 ],
-         [-1.5 ,2.3 ]]
-bws = [2.5, 0.5, 0.5]
+datacols = ['model_alpha','model_beta','model_theta']
 cCount = len(datacols)
 
 for i in range(cCount):
     # plot
-    sns.violinplot(ax=axs_bottom[i], x="Group", y=datacols[i], data=dfsubs, palette=groupcolors, bw=bws[i])
+    sns.violinplot(ax=axs_bottom[i], x="group", y=datacols[i], data=dfsubs, palette=groupcolors)
     patch_violinplot(axs_bottom[i])
-    axs_bottom[i].set_ylim(ylims[i][0],ylims[i][1])
     axs_bottom[i].set_ylabel('') 
     axs_bottom[i].set_xlabel('')
     axs_bottom[i].set_xticks([],[])
